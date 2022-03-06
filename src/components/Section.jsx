e95e173d4a73cc965381882b9545c0d5a734a5b5
henrysama58 <henrysama58@gmail.com>
2022-03-06 02:07:01 -0500

    working on styled components landing page

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,6 +8,17 @@ const Section = () => {
                 <h1>Model 3</h1>
                 <p>Order Online for <span>Touchless Delivery</span></p>
             </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        CUSTOM ORDER
+                    </LeftButton>
+                    <RightButton>
+                        EXISTING INVENTORY
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="assets/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     );
 }
@@ -21,9 +32,47 @@ const Wrap = styled.div`
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
 
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 32px;
+`
+
+const LeftButton = styled.div`
+    background-color: #171A20CC;
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    font-size: 12px;
+    cursor: pointer;
+    margin-right: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: #f4f4f4a6;
+    color: #393c41;
+`
+
+const DownArrow = styled.img`
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+    overflow-x: hidden;
+`
+
+const Buttons = styled.div`
+    
 `
